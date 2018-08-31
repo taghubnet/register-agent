a2e7130de372017c61a773b207e87bb4feb73dab
Asbjørn Enge <asbjorn@hanafjedle.net>
2018-08-31 14:11:12 +0200

    Did a new build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 VERSION=$(cat package.json | jq -r .version)
 npm run build
-docker build -t taghub:5000/register-agent:$VERSION .
+docker build -t registry.taghub.net:5000/register-agent:$VERSION .
