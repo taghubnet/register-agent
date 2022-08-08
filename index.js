592c7d24d644040f3b696d170914b273a6caae53
Pål Sola <pal@smartm.no>
2022-08-08 14:05:41 +0200

    fixed issues with nodes already joined to swarm

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ function detectAndUpdate() {
           return log(err)
         }
         let r = new RegExp("This node is already part of a swarm")
-        if (res.statusCode != 200 && !r.test(res.message)) {
+        if (res.statusCode != 200 && !r.test(rrpayload)) {
           log(rrpayload) 
           log(res.message)
           log("Not success, retrying...")
